94d810be3e6fbefc3b25f7cf84bbcd388f21b9e2
Scuti152 <jquinlan152@gmail.com>
2025-12-01 21:04:51 -0600

    Update book_search.py

--- src/book_search.py
+++ src/book_search.py
@@ -1,101 +1,32 @@
-import csv, os, mysql.connector
+import csv, mysql.connector
 
-def book_search(search, books, authors, book_authors):
-    matches = []
+def book_search(search):
 
-    with open(books, mode='r', newline='', encoding='utf-8') as f:
+    db = mysql.connector.connect(user='root',
+        password='password')
+    cursor = db.cursor()
 
-        reader = csv.DictReader(f)
+    cursor.execute('USE Library;')
 
-        for row in reader:
-            title = row['Title']
-            isbn = row['Isbn']
-            if search.casefold() in title.casefold():
-                matches.append([isbn, title, '', '', ''])
-            if search.casefold() in isbn.casefold():
-                matches.append([isbn, title, '', '', ''])
+    cursor.execute('SELECT *'
+                   'FROM books '
+                   'INNER JOIN book_authors ON books.isbn = book_authors.isbn '
+                   'INNER JOIN authors ON authors.author_id = book_authors.author_id'
+                   'WHERE books.isbn LIKE \'%' + search + '%\''
+                   '    OR books.title LIKE \'%' + search + '%\''
+                   '    OR authors.name LIKE \'%' + search + '%\';')
 
-    with open(authors, mode='r', newline='', encoding='utf-8') as f:
 
-        reader = csv.DictReader(f)
 
-        for row in reader:
-            a_id = row['Author_id']
-            name = row['Name']
-            if search.casefold() in name.casefold():
-                matches.append(['', '', a_id, name, ''])
-            if search.casefold() in a_id.casefold():
-                matches.append(['', '', a_id, name, ''])
+    print("NO   ISBN       TITLE                                             AUTHORS                                        BORROWED")
+    count = 1
+    for (books.isbn, books.title, authors.name, books.borrowed) in cursor:
+        print("%d {} {} {} {}".format(count, books.isbn, books.title, authors.name, books.borrowed), count)
 
-    with open(book_authors, mode='r', newline='', encoding='utf-8') as f:
-
-        reader = csv.DictReader(f)
-
-        for row in reader:
-            a_id = row['Author_id']
-            isbn = row['Isbn']
-            for book in matches:
-                if book[2] == a_id:
-                    book[0] = isbn
-                if book[0] == isbn:
-                    book[2] = a_id
-
-    with open(books, mode='r', newline='', encoding='utf-8') as f:
-
-        reader = csv.DictReader(f)
-
-        for row in reader:
-            isbn = row['Isbn']
-            title = row['Title']
-            for book in matches:
-                if book[0] == isbn:
-                    book[1] = title
-
-    with open(authors, mode='r', newline='', encoding='utf-8') as f:
-
-        reader = csv.DictReader(f)
-
-        for row in reader:
-            a_id = row['Author_id']
-            name = row['Name']
-            for book in matches:
-                if book[2] == a_id:
-                    book[3] = name
-
-    curCheck = 0
-    curBook = 0
-    combinedIsbns = []
-    results = []
-    for row in matches:
-        if row[0] in combinedIsbns:
-            continue
-        for book in matches:
-            if book[0] in combinedIsbns:
-                curBook += 1
-            elif curBook <= curCheck:
-                curBook += 1
-            elif isbn == book[0]:
-                row[2] = row[2] + ', ' + book[2]
-                row[3] = row[3] + ', ' + book[3]
-                combinedIsbns.append(book[0])
-            else:
-                curBook += 1
-        results.append(row)
-        curCheck += 1
-
-    print('ISBN       TITLE                                                                                                                                                                 AUTHOR IDS          AUTHORS                       STATUS')
-    for row in results:
-        if not row:
-            next(row)
-        print("{: >10} {: >150} {: >20} {: >30} {: >6}".format(*row))
 
 
 if __name__ == "__main__":
-    # Define file paths
-    books = '../normalized_data/normalized_book.csv'
-    authors = '../normalized_data/normalized_authors.csv'
-    book_authors = '../normalized_data/normalized_book_authors.csv'
 
     # Search data
     search = input()
-    book_search(search, books, authors, book_authors)
+    book_search(search)
