ca5ba41d44067b3f79e906274a8bec4c4ad2dc50
Scuti152 <jquinlan152@gmail.com>
2025-12-11 00:01:08 -0600

    Update frontend.py

--- src/frontend.py
+++ src/frontend.py
@@ -11,7 +11,20 @@ def search():
 
     book_search.book_search(query)
 
+def borrowers():
+    borrowers = Tk()
+    frm = ttk.Frame(borrowers, padding=20)
+    frm.grid()
+    ttk.Label(frm, text="Borrowers").grid(column=0, row=0)
+    ttk.Button(frm, text="Get Fines", command=fines.list_fines).grid(column=0, row=1)
+    ttk.Button(frm, text="Pay Fines", command=payfines).grid(column=0, row=2)
+    ttk.Button(frm, text="Quit", command=root.destroy).grid(column=0, row=3)
+    root.mainloop()
 
+def payfines():
+    query = tkinter.simpledialog.askstring("Card ID", "")
+
+    fines.pay_fines(query)
 
 if __name__ == "__main__":
     root = Tk()
@@ -19,6 +32,6 @@ if __name__ == "__main__":
     frm.grid()
     ttk.Label(frm, text="Library System").grid(column=0, row=0)
     ttk.Button(frm, text="Search", command=search).grid(column=0, row=1)
-    ttk.Button(frm, text="Borrowers", command=root.destroy).grid(column=0, row=2)
+    ttk.Button(frm, text="Borrowers", command=borrowers).grid(column=0, row=2)
     ttk.Button(frm, text="Quit", command=root.destroy).grid(column=0, row=3)
     root.mainloop()
