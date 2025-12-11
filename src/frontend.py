44f4d701eb853c9035fc22d1ab26ba8f0c0b0928
Scuti152 <jquinlan152@gmail.com>
2025-12-10 23:53:22 -0600

    frontend search

--- src/frontend.py
+++ src/frontend.py
@@ -1,15 +1,24 @@
+import tkinter
+import tkinter.simpledialog
 from tkinter import *
 from tkinter import ttk
 import book_search
 import fines
 import borrower_management
 
+def search():
+    query = tkinter.simpledialog.askstring("Search", "")
+
+    book_search.book_search(query)
+
+
+
 if __name__ == "__main__":
     root = Tk()
-    frm = ttk.Frame(root, padding=10)
+    frm = ttk.Frame(root, padding=20)
     frm.grid()
     ttk.Label(frm, text="Library System").grid(column=0, row=0)
-    ttk.Button(frm, text="Search", command=root.destroy).grid(column=0, row=1)
+    ttk.Button(frm, text="Search", command=search).grid(column=0, row=1)
     ttk.Button(frm, text="Borrowers", command=root.destroy).grid(column=0, row=2)
     ttk.Button(frm, text="Quit", command=root.destroy).grid(column=0, row=3)
     root.mainloop()
